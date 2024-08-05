81ad8a95a16dd55193d1e729ca9c1280f17b0c69
Andressa Isidoro <dessa_silva93@outlook.com>
2024-08-05 15:30:12 -0300

    data e hora

--- scripts.js
+++ scripts.js
@@ -1,65 +1,72 @@
-const button = document.querySelector('.button-add-task')
-const input = document.querySelector('.input-task')
-const listaCompleta = document.querySelector('.list-tasks')
+const button = document.querySelector('.button-add-task');
+const input = document.querySelector('.input-task');
+const inputDate = document.querySelector('.input-date');
+const inputTime = document.querySelector('.input-time');
+const listaCompleta = document.querySelector('.list-tasks');
 
-let minhaListaDeItens = []
+let minhaListaDeItens = [];
 
+// Função para adicionar uma nova tarefa
 function adicionarNovaTarefa() {
   minhaListaDeItens.push({
     tarefa: input.value,
+    data: inputDate.value,
+    hora: inputTime.value,
     concluida: false,
-  })
+  });
 
-  input.value = ''
+  input.value = '';
+  inputDate.value = '';
+  inputTime.value = '';
 
-  mostrarTarefas()
+  mostrarTarefas();
 }
 
+// Função para mostrar as tarefas na lista
 function mostrarTarefas() {
-  let novaLi = ''
-
-  // ['comprar café', 'estudar programação']
+  let novaLi = '';
 
   minhaListaDeItens.forEach((item, posicao) => {
-    novaLi =
-      novaLi +
-      `
+    novaLi += `
+      <li class="task ${item.concluida ? 'done' : ''}">
+        <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+        <p>${item.tarefa}</p>
+        <span>${item.data} ${item.hora}</span>
+        <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+      </li>
+    `;
+  });
 
-        <li class="task ${item.concluida && 'done'}">
-            <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
-            <p>${item.tarefa}</p>
-            <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
-        </li>
-        
-        `
-  })
+  listaCompleta.innerHTML = novaLi;
 
-  listaCompleta.innerHTML = novaLi
-
-  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+  // Armazenar a lista de tarefas no Local Storage
+  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens));
 }
 
+// Função para marcar uma tarefa como concluída
 function concluirTarefa(posicao) {
-  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
-
-  mostrarTarefas()
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida;
+  mostrarTarefas();
 }
 
+// Função para deletar uma tarefa
 function deletarItem(posicao) {
-  minhaListaDeItens.splice(posicao, 1)
-
-  mostrarTarefas()
+  minhaListaDeItens.splice(posicao, 1);
+  mostrarTarefas();
 }
 
+// Função para recarregar as tarefas do Local Storage
 function recarregarTarefas() {
-  const tarefasDoLocalStorage = localStorage.getItem('lista')
+  const tarefasDoLocalStorage = localStorage.getItem('lista');
 
   if (tarefasDoLocalStorage) {
-    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage);
   }
 
-  mostrarTarefas()
+  mostrarTarefas();
 }
 
-recarregarTarefas()
-button.addEventListener('click', adicionarNovaTarefa)
+// Inicializar a lista de tarefas ao carregar a página
+recarregarTarefas();
+button.addEventListener('click', adicionarNovaTarefa);
+
